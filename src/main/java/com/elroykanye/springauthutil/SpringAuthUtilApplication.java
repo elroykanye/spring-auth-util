f4326f304a7ab828422bdcc476f659dc8a343eac
Elroy Kanye <elroykanye@gmail.com>
2022-03-01 10:09:26 +0100

    initial commit

---
+++ src/main/java/com/elroykanye/springauthutil/SpringAuthUtilApplication.java
@@ -0,0 +1,13 @@
+package com.elroykanye.springauthutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthUtilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elroykanye/springauthutil/SpringAuthUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elroykanye.springauthutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
